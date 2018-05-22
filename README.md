a2e6e3841cb08f1190a2c7cfe5abb6172ca43c32
Bobby Manuel <bobby@ginger.io>
2018-05-22 11:47:58 -0700

    Initial import of client upload tools.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # gingerbox
+
 Membership and eligibility file upload tool for ginger.io clients and partners.
+
+## Sending files programmatically: 
+
+If you wish to automate your eligibility file transmission, your Ginger.io account manager can send you a utility called gingerbox_upload.sh.  This utility will be accompanied with a credentials.txt file which is sent separately for security purposes.  The utility should work from the command line of any unix or linux machine and can be easily automated.  To upload the file to your google drive, save the credentials.txt file on your computer and use the syntax indicated below to point the uploader at both the credentials file, and the file you wish to upload.  See example command below: 
+
+```
+$> ./gingerbox_upload.sh --config credentials.txt file_to_upload.csv 
+```
+
+The file_to_upload.csv will be uploaded to your google drive account’s Eligibility file upload folder (see _Sending files with Google Drive_ below).  Processing and error reporting will be handled identically to manually uploaded files via google drive.
+
+## What encryption methods does Ginger.io support?
+
+We recommend that you encrypt your files before sending them to us.  Encrypt the file with gpg and the provided Ginger.io public key.  Be sure to append the additional file extension “.gpg” to the file so that we can identify and correctly route the file for secure storage and decryption.  
+
+## Do files have to be encrypted before uploading them to Ginger.io?
+
+Though we strongly encourage file level encryption, all of our delivery methods are encrypted and comply with HIPAA standards.  If you do not wish to encrypt your files prior to transmission we will still accept your unencrypted file via the secure transmission methods we describe in this document. 
+
+## Sending files with Google Drive
+
+Your account manager will send you an invitation by email that will give you access to your google drive folder <your_org>@gingerbox.io.  
+
+Once you have signed in to Google drive with your gingerbox.io login, you can simply drag your eligibility file into the folder named _Eligibility file upload_.  After the upload completes, the uploaded file will be processed within the hour and will disappear from your google drive _Eligibility upload folder_.  If there was a problem with the file, you will receive a failure notification by email with a link to a document describing issues with the file that may need to be amended.
+
---
+++ gingerbox_upload.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Upload an eligibility file to gingerbox.io
+#
+# Note : Your .gingerbox.conf file should be copied to your home directory,
+#        or specified via the '--config' command option.
+#
+# Usage: gingerio_upload.sh <file>
+
+#!/bin/bashset -e
+
+function usage(){
+    echo -e "\nThe script can be used to upload file to your gingerbox account."
+    echo -e "\nUsage:\n $0 [options..] <filename> \n"
+    echo -e "Options:"
+    echo -e "-h | --help - Display usage instructions."
+    echo -e "-z | --config - Specify the location of the config file. (Default: ${HOME}/.gingerbox.conf).\n"
+    exit 0;
+}
+
+# Default location of upload credentials.
+CONFIG="${HOME}/.gingerbox.conf"
+
+ACCESS_TOKEN=""
+FILE=""
+curl_args=" --progress"
+
+set -o errexit -o noclobber -o pipefail
+
+if [ ! -z "$CONFIG" ]
+    then
+    if [ -e "$CONFIG" ]
+    then
+    . ${CONFIG}
+    fi
+fi
+
+# Unpack args.
+while true; do
+  case "$1" in
+    -h | --help ) usage; shift ;;
+    -z | --config ) CONFIG="$2"; shift 2 ;;
+    * ) echo "case *"; break ;;
+  esac
+done
+
+if [ ! -z "$1" ]
+then
+    FILE=$1
+else
+    echo "FILE is not optional"
+    usage;
+    exit -1;
+fi
+
+# Method to extract data from json response.
+function jsonValue() {
+KEY=$1
+num=$2
+awk -F"[,:}][^://]" '{for(i=1;i<=NF;i++){if($i~/\042'${KEY}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p | sed -e 's/[}]*$//' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[,]*$//'
+}
+
+function log() {
+    echo -e "${1}"
+}
+
+# Method to upload files to google drive.
+# Requires 3 arguments file path, google folder id and access token.
+function uploadFile(){
+
+    FILE="$1"
+    FOLDER_ID="$2"
+    ACCESS_TOKEN="$3"
+    SLUG=`basename "$FILE"`
+    FILESIZE=$(wc -c "$FILE" | awk '{print $1}')
+
+    # JSON post data to specify the file name and containing folder.
+    postData="{\"name\": \"$SLUG\",\"parents\": [\"$FOLDER_ID\"]}"
+    postDataSize=$(echo ${postData} | wc -c)
+
+    # Curl command to initiate resume-able upload session and grab the returned location URL.
+    log "Generating upload link for file $FILE ..."
+    uploadLink=`/usr/bin/curl \
+                --silent \
+                -X POST \
+                -H "Host: www.googleapis.com" \
+                -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+                -H "Content-Type: application/json; charset=UTF-8" \
+                -H "X-Upload-Content-Length: $FILESIZE" \
+                -d "$postData" \
+                "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable" \
+                --dump-header - | sed -ne s/"Location: "//p | tr -d '\r\n'`
+
+    # Curl command to push the file to ginger box.
+    # If the file size is large then the content can be split to chunks and uploaded.
+    # In that case content range needs to be specified.
+    log "Uploading file $FILE to google drive..."
+    curl \
+    -X PUT \
+    -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -H "Content-Length: ${FILESIZE}" \
+    -H "Slug: ${SLUG}" \
+    --upload-file "$FILE" \
+    --output /dev/null \
+    "$uploadLink" \
+    ${curl_args}
+}
+
+# Get an access token from the refresh token found in config.
+if [ -z "$ACCESS_TOKEN" ]
+then
+    RESPONSE=`curl --silent "https://www.googleapis.com/oauth2/v4/token" --data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token&refresh_token=$REFRESH_TOKEN"`
+    ACCESS_TOKEN=`echo "$RESPONSE" | jsonValue access_token`
+fi
+
+# Upload the file.
+if [ ! -z "$FILE" ]; then
+    if [ -f "$FILE" ];then
+        uploadFile "$FILE" "$FOLDER_ID" "$ACCESS_TOKEN"
+        exit 0
+    fi
+fi
+
+# Invalid file, exit with error and usage.
+log "A valid file must be specified."
+usage
+exit -1
